48e2f7265c3bbe0cf9b1491a336388bcee1cbf4b
slawek <slawomir0211@gmail.com>
2017-10-25 16:11:27 +0200

    changed to IpCalc class; added IpCalcCli class fo cli command

---
+++ src/IpCalcCli.java
@@ -0,0 +1,23 @@
+public class IpCalcCli {
+
+    public static void main(String[] args){
+
+        if(args.length != 0) {
+            System.out.println("IpCalc for    : " + args[0]);
+            IpCalc myip = new IpCalc(args[0]);
+
+            System.out.println("Address IP    : " + myip.getIpAddress());
+            System.out.println("Netmask       : " + myip.getNetmask());
+            System.out.println("Network       : " + myip.getNetwork());
+            System.out.println("Address Min   : " + myip.getAddressMin());
+            System.out.println("Address Max   : " + myip.getAddressMax());
+            System.out.println("Broadcast     : " + myip.getBroadcast());
+            System.out.println("Address Count : " + myip.getAddressCount());
+        }
+        else{
+            System.out.println("IP Address is empty!");
+        }
+
+    }
+
+}
